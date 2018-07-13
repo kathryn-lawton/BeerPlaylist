69cef119e7c381bdc46314ee80636f686ad470f6
Scott Tracy <scott.tracy.st@gmail.com>
2018-07-13 08:17:54 -0500

    seeded roles

--- BeerPlaylist/Global.asax.cs
+++ BeerPlaylist/Global.asax.cs
@@ -13,9 +13,16 @@ namespace BeerPlaylist
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            IdentityHelper.SeedIdentities(context);
         }
     }
 }
---
+++ BeerPlaylist/IdentityHelper.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BeerPlaylist.models;
+
+namespace Trash_Collector
+{
+    public class IdentityHelper
+    {
+        
+
+        public const string ROLE_Employee = "Admin";
+
+        public const string ROLE_Customer = "Customer";
+
+        internal static void SeedIdentities(System.Data.Entity.DbContext context)
+
+        {
+
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            if (!roleManager.RoleExists(ROLE_Employee))
+
+            {
+
+                var roleresult = roleManager.Create(new IdentityRole(ROLE_Employee));
+
+            }
+
+            if (!roleManager.RoleExists(ROLE_Customer))
+
+            {
+
+                var roleresult = roleManager.Create(new IdentityRole(ROLE_Customer));
+
+            }
+
+        }
+
+
+    }
+}
